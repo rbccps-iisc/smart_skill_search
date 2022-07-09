8738111fd1bfd73633297c9f21a0ff9636edc08d
joji <jojinattar@gmail.com>
2022-07-09 17:37:38 +0530

    Implemented a knn model

--- POC/knn_model.py
+++ POC/knn_model.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.metrics import auc
 from sklearn.naive_bayes import BernoulliNB
 import sklearn.metrics
+from tqdm import tqdm
 
 with open("./datasets/resumes_pilot.csv", "rb") as f:
     dataset = pd.read_csv(f)
@@ -17,27 +18,42 @@ X_train, X_test, y_train, y_test = train_test_split(dataset, dataset, test_size=
 
 # print(len(X_train), len(X_test))
 model_knn = NearestNeighbors(metric=sklearn.metrics.hamming_loss)
-model_knn.fit(X_train)
+model_knn.fit(X_train.values)
 
 
 class KNNRecommender:
-    def __init__(self):
-        self.model_knn = NearestNeighbors(metric=sklearn.metrics.hamming_loss)
+    def __init__(self, k=5, threshold=2):
+        self.model_knn = NearestNeighbors(metric="manhattan", n_neighbors=k)
+        self.k = k
+        self.threshold = threshold
     def fit(self, X):
-        self.model_knn.fit(X)
+        self.model_knn.fit(X.values)
         self.X = X
     def recommend(self, row):
         dist, indices = self.model_knn.kneighbors([row])
         rec_matrix = np.array([self.X.iloc[idx] for idx in indices])
-        print(rec_matrix)
-        print(indices)
-        return rec_matrix
+        recommendation = 1 * (rec_matrix.squeeze().sum(axis=0) > self.threshold)
+        return recommendation
+
+    def get_batch_recommendation(self, X):
+
+        batch = []
+        idx = 0
+        for row in tqdm(X.iterrows(), total = len(X)):
+            idx += 1
+            batch.append(self.recommend(row[1]))
+        return np.array(batch)
+
+    
 
 model = KNNRecommender()
 model.fit(X_train)
-rec = model.recommend(X_train.iloc[0]).squeeze()
-print(rec.shape)
-threshold = 1
-rec_add = 1*(rec.sum(axis=0) > threshold)
-print(rec_add == np.array(X_train.iloc[0]))
-np.savetxt("rec.txt", rec.squeeze().astype(np.int8))
+mtx = model.get_batch_recommendation(X_train)
+# print(batch.shape)
+print(mtx.shape)
+# rec = model.recommend(X_train.iloc[0]).squeeze()
+# print(rec.shape)
+# threshold = 1
+# rec_add = 1*(rec.sum(axis=0) > threshold)
+# print(rec_add == np.array(X_train.iloc[0]))
+# np.savetxt("rec.txt", rec.squeeze().astype(np.int8))
