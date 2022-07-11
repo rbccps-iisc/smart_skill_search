ba714b26405299ffa89f6fc751374e2e8b05d8d9
acharya <srikrishna3118@gmail.com>
2022-07-11 16:30:12 +0530

    database update with error need fixing

--- app/utils/init_db.js
+++ app/utils/init_db.js
@@ -2,14 +2,19 @@
 const csvtojson = require('csvtojson');
 var db_c = require('../models/db');
 
-const fileName = "/home/acharya/test/csv_python/combined.csv";
+const fileName = "/home/acharya/test/csv_python/combined_2.csv";
 
 exports.get = function (req, res) {
     csvtojson()
         .fromFile(fileName)
         .then((jsonObj) => {
             console.log(jsonObj);
-            db_c.insert(jsonObj, function (err, result) {
+            let data = [];
+            //FIXME: fixed to clear the db before update.
+            db_c.clear((err, result) => {
+                data.push(jsonObj);
+            });
+            db_c.insert(data, function (err, result) {
                 if (err)
                     res.send(500);
                 else {
